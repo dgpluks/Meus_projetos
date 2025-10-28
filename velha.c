fd4fa60091d4760d1b324d7fcac89fd8dbf0e7d0
Daniel Gomes Pereira <danielgomespereira2006@gmail.com>
2025-10-28 16:46:23 -0300

    Update velha.c

--- velha.c
+++ velha.c
@@ -35,13 +35,13 @@ int verificar (char ***mat){
 	}
 	if((*mat)[16][7] != ' ' && (*mat)[16][7] == (*mat)[10][19] && (*mat)[10][19] == (*mat)[4][31]){
 		return qual((*mat)[16][7]);
-	}else if((*mat)[4][7] != ' ' && (*mat)[4][7] == (*mat)[10][19] && (*mat)[16][31] == (*mat)[16][31]){
+	}else if((*mat)[4][7] != ' ' && (*mat)[4][7] == (*mat)[10][19] && (*mat)[10][19] == (*mat)[16][31]){
 		return qual((*mat)[4][7]);
 	}
 	return 0;
 }
 bool inserir(char ****mat, int pos, bool tipo, int i){
-//	system("clear");
+	system("clear");
 	if((*mat)[i][4+6*(pos/3)][7+12*(pos%3)] == ' '){
 		for(; i < 9; i++)
 			x_o(tipo, &((*mat)[i]), 2+6*(pos/3), 5+12*(pos%3));
@@ -50,88 +50,99 @@ bool inserir(char ****mat, int pos, bool tipo, int i){
 	return true;
 }
 int main(){
-//	system("clear");
-	char ***jogo = malloc(sizeof(char**)*9);
-	for(int i = 0; i < 9; i++){
-		jogo[i] = malloc(sizeof(char*)*17);
-		for(int j = 0;j < 17; j++ ){
-			jogo[i][j] = malloc(sizeof(char)*35);
-			for(int k = 0; k < 35; k++){
-				if(j == 5 && k == 11 || j == 11 && k == 11 || j == 5 && k == 23 || j == 11 && k == 23){
-					jogo[i][j][k] = '+';
-				}else if(j == 5 || j == 11){
-					jogo[i][j][k] = '-';
-				}else if(k == 11 || k == 23){
-					jogo[i][j][k] = '|';
-				}else{
-					jogo[i][j][k] = ' ';
+	int x = -2;
+	char ***jogo = malloc(sizeof(char**)*10);
+	while(x == -2){
+		system("clear");  //limpa o terminal
+		for(int i = 0; i < 10; i++){   //reinicia o tabuleiro de cada uma das jogadas das rodadas
+			jogo[i] = malloc(sizeof(char*)*17);
+			for(int j = 0;j < 17; j++ ){  //inicia as linhas
+				jogo[i][j] = malloc(sizeof(char)*35);
+				for(int k = 0; k < 35; k++){  //inicia as celulas das linhas
+					if(j == 5 && k == 11 || j == 11 && k == 11 || j == 5 && k == 23 || j == 11 && k == 23){
+						jogo[i][j][k] = '+';
+					}else if(j == 5 || j == 11){
+						jogo[i][j][k] = '-';
+					}else if(k == 11 || k == 23){
+						jogo[i][j][k] = '|';
+					}else{
+						jogo[i][j][k] = ' ';
+					}
 				}
 			}
 		}
-	}
-	int vencedor = 0;
-	bool tipo = true;  //true = X false = O
-	char insert = ' ';
-	int i = 0;
-	for(; i < 9 && vencedor == 0; i++){
-		for(int j = 0;j < 17 && vencedor == 0; j++ ){
-			for(int k = 0; k < 35 && vencedor == 0; k++){
-				printf("%c", jogo[i][j][k]);
+		int vencedor = 0;
+		bool tipo = true;  //true = X false = O
+		char insert = ' ';
+		int i = 1;
+		for(; i < 10 && vencedor == 0; i++){  //executa cada uma das jogadas das 9 rodadas ou até alguem ganhar
+			for(int j = 0;j < 17 && vencedor == 0; j++ ){  //printa o tabuleiro atual
+				for(int k = 0; k < 35 && vencedor == 0; k++){
+					printf("%c", jogo[i][j][k]);  
+				}
+				printf("\n");
 			}
-			printf("\n");
-		}
-		scanf(" %c", &insert);
-		if(insert >= 'a' && insert <= 'i'){
-			if(inserir(&jogo, (int)(insert - 'a'), tipo, i)){
-				i--;
-				printf("A posição %c ja está preenchida\n", insert);
+			scanf(" %c", &insert);
+			if(insert >= 'a' && insert <= 'i'){
+				if(inserir(&jogo, (int)(insert - 'a'), tipo, i)){
+					i--;
+					printf("A posição %c ja está preenchida\n", insert);
+				}else{
+					vencedor = verificar(&(jogo[i]));
+				}
+			}else if(insert>= 'A' && insert <= 'I'){
+				if(inserir(&jogo, (int)(insert - 'A'), tipo, i)){
+					i--;
+					printf("A posição %c ja está preenchida\n", insert);
+				}else{
+					vencedor = verificar(&(jogo[i]));
+				}
 			}else{
-				vencedor = verificar(&(jogo[i]));
-			}
-		}else if(insert>= 'A' && insert <= 'I'){
-			if(inserir(&jogo, (int)(insert - 'A'), tipo, i)){
+				system("clear");
+				printf("\033[1;31m!!!DIGITE UM VALOR VÁLIDO (A - I)!!!\033[0m\n");
 				i--;
-				printf("A posição %c ja está preenchida\n", insert);
-			}else{
-				vencedor = verificar(&(jogo[i]));
 			}
-		}else{
-//			system("clear");
-			printf("\033[1;31m!!!DIGITE UM VALOR VÁLIDO (A - I)!!!\033[0m\n");
-			i--;
+			tipo = !tipo;
 		}
-		tipo = !tipo;
-	}	
-	for(int j = 0;j < 17; j++ ){
-        	for(int k = 0; k < 35; k++){
-                	if(j == 5 && k == 11 || j == 11 && k == 11 || j == 5 && k == 23 || j == 11 & k == 23){
-				jogo[i][j][k] = '+';
-                        }else if(j == 5 || j == 11){
-				jogo[i][j][k] = '-';
-                        }else if(k == 11 || k == 23){
-				jogo[i][j][k] = '|';
-                        }else{
-				jogo[i][j][k] = ' ';
-                        }
-                }
-        }
-	int x = -1;
-	scanf(" %d", &x);
-	while(x != 0){
-		system("clear");
 		if(vencedor == 1)
 			printf("VENCEDOR FOI O X\n");
 		else if(vencedor == 2)
 			printf("VENCEDOR FOI O O\n");
 		else
-			printf("DEU VELHA\n");
+				printf("DEU VELHA\n");
+		if(i == 10)
+			i--;
 		for(int j = 0;j < 17; j++ ){
 			for(int k = 0; k < 35; k++){
-				printf("%c", jogo[x-1][j][k]);
+				printf("%c", jogo[i][j][k]);
 			}
 			printf("\n");
 		}
+		printf("Recomeçar -->  Digite -2\nFinalizar --> Digite -1\nAnalizar jogadas --> Digite valores de 1-10\n");
 		scanf(" %d", &x);
+		while(x != -1 && x != -2 && x <= i){
+			system("clear");
+			if(!(x >i || x < 0)){
+				if(x == i){
+					if(vencedor == 1)
+						printf("VENCEDOR FOI O X\n");
+					else if(vencedor == 2)
+						printf("VENCEDOR FOI O O\n");
+					else
+						printf("DEU VELHA\n");
+				}
+				for(int j = 0;j < 17; j++ ){
+					for(int k = 0; k < 35; k++){
+						printf("%c", jogo[x][j][k]);
+					}
+					printf("\n");
+				}
+			}else{
+				printf("Digite um valor válido\n");
+			}	
+			printf("Recomeçar -->  Digite -2\nFinalizar --> Digite -1\nAnalizar jogadas --> Digite valores de 1-10\n");
+			scanf(" %d", &x);
+		}
 	}
 }
 
@@ -154,3 +165,4 @@ int main(){
            |             |
            |             |  
 	 */
+
