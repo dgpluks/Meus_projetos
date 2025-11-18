806e8276771d4196887ff534d847853f33ddfeb2
Daniel Gomes Pereira <danielgomespereira2006@gmail.com>
2025-11-18 02:39:16 -0300

    Update Longest_Substring_Without_Repeating_Characters.c

--- Longest_Substring_Without_Repeating_Characters.c
+++ Longest_Substring_Without_Repeating_Characters.c
@@ -1,22 +1,27 @@
 //https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
 int insert(char s,int i , int **posi){
-    if((*posi)[s] == -1){
-        (*posi)[s] = i;
-        return 1;
-    }else{
+    if((*posi)[s] != -1){
+        //printf("1 -- %c\n", s);
         return 0;
     }
+    (*posi)[s] = i;
+    return 1;
 }
 int lengthOfLongestSubstring(char* s) {
     int *posi = malloc(300*sizeof(int)), tam = 0, i = 0;
     for(int j = 0; j < 300; j++){
         posi[j] = -1;
     }
+    int resp = 0;
     while(s[i] != '\0'){
+       // printf("%d~ ",resp);
         if(insert(s[i],i, &posi)){
+            //printf("1 -- %c\n", s[i]);
+           //printf("(%d)\n", tam);
             tam++;
             i++;
         }else{
+            //printf("2 -- %c\n", s[i]);
             int *posi2 = malloc(300*sizeof(int));
             for(int j = 0; j < 300; j++){
                 posi2[j] = -1;
@@ -25,19 +30,28 @@ int lengthOfLongestSubstring(char* s) {
                 insert(s[j], j, &posi2);
             }
             int aux = i - posi[s[i]];
+            printf("(-i = %d  s[i] = %c-    posi[s[i]] = %d)\n", i, s[i], posi[s[i]]);
             i++;
-            while(s[i] != '\0' && insert(s[i],i, &posi)){
+            while(s[i] != '\0' && insert(s[i],i, &posi2)){
+                printf("%c", s[i]);
                 i++;
                 aux++;
             }
-            if(aux > tam){
+            
+            if(aux >= tam){
+                //printf("troca\n");
                 tam = aux;
                 free(posi);
                 posi = posi2;
-                posi2 = NULL;
-                free(posi2);
             }
+            posi2 = NULL;
+            free(posi2);
+            if(tam > resp)
+                resp = tam;
+            tam = 0;
         }
     }
-    return tam;
+    if(resp == 0)
+        return tam;
+    return resp;
 }
